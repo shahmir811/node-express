fd4f05a8473e7e9b37e575092afc4297af0f7eb6
shahmir811 <shahmirkj@gmail.com>
2017-07-04 18:43:36 +0500

    First Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Following is the handler for HTTP get requests
+app.get('/', (req, res) => {
+
+  //res.send('<h1>Hello Express</h1>');
+  res.send({
+    name: 'Shahmir Khan Jadoon',
+    hobbies: ['Swimming', 'Sports', 'Programming', 'Coding']
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About Page</h1>')
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'OOPs Page not found'
+  });
+});
+
+
+app.listen(port);
