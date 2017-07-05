43bd9c9b213d05dd5539ee5cfcc34966766311e6
shahmir811 <shahmirkj@gmail.com>
2017-07-05 10:19:52 +0500

    44 - Advanced Templating

--- server.js
+++ server.js
@@ -2,24 +2,38 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
+hbs.registerPartials(__dirname + '/views/partials'); // Register Partials
 app.set('view engine', 'hbs'); // set view engine to hbs
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+// app.use( (req, res, next) => {
+//   var now = new Date().toString(); // gives human readable timestamp
+//   console.log(`${now}: `);
+//
+//   next();
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('Upsize', (text) => {
+  return text.toUpperCase();
+});
+
 // Following is the handler for HTTP get requests
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeNote: 'Welcome to Our Website About Node.JS.'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()// return current year(2017)
+    pageTitle: 'About Page'
   }) // let you render any of the template you have setup with your view engine. Views directory are default(no need to mention here). But you can overrite it
 });
 
