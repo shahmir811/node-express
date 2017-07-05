fc0e5690348f3438096d20dcc3bc36f23c021f2b
shahmir811 <shahmirkj@gmail.com>
2017-07-05 08:41:29 +0500

    43 - Rendering Data with Template Engine

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+app.set('view engine', 'hbs'); // set view engine to hbs
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 // Following is the handler for HTTP get requests
 app.get('/', (req, res) => {
-
-  //res.send('<h1>Hello Express</h1>');
-  res.send({
-    name: 'Shahmir Khan Jadoon',
-    hobbies: ['Swimming', 'Sports', 'Programming', 'Coding']
-  })
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeNote: 'Welcome to Our Website About Node.JS.'
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h1>About Page</h1>')
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()// return current year(2017)
+  }) // let you render any of the template you have setup with your view engine. Views directory are default(no need to mention here). But you can overrite it
 });
 
 app.get('/bad', (req, res) => {
